112a8eb7002aaa285d5b1d20b41bc737435f4aaf
prasad <prasad.joshi1@gmail.com>
2020-05-01 20:15:11 +0530

    more navigation and screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
+import './screens/meal_detail_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -33,6 +35,19 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(), 
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+      },
+      /* onGenerateRoute: (settings) {
+        print(settings.arguments);
+        if(settings.name == '/'){
+          return ...;
+        }else if(settings.name == '/something-else') {
+          return ...;
+        }
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen()); */
+      //},
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen()); 
       },
     );
   }

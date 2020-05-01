f9d04572517dd67b1837f497cec6233d3fdf4bc0
prasad <prasad.joshi1@gmail.com>
2020-05-01 22:34:11 +0530

    added TabBar  and Faorties

---
+++ lib/screens/favorities_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class FavortiesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('The Favorties !!'),
+    );
+  }
+}

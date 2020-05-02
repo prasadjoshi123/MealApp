c1ff31469c39c794d3bb3b29208e40012ab99039
prasad <prasad.joshi1@gmail.com>
2020-05-02 09:12:57 +0530

    filters and more

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './categories_screen.dart';
 import './favorities_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -9,12 +10,53 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  
+  final List<Map<String, Object>> _pages = [
+    {'page' : CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavortiesScreen(), 'title': 'Favorties'}
+];
+
+  int _selectedPageIndex = 0;
+  
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
+    // return DefaultTabController(
+    //   length: 2,
+    //   child:
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        //type: BottomNavigationBarType.fixed,
+
+        onTap: _selectPage,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
+      ),
+      /*     appBar: AppBar(
           title: Text('Meals'),
           bottom: TabBar(
             tabs: [
@@ -34,8 +76,8 @@ class _TabsScreenState extends State<TabsScreen> {
             CategoriesScreen(),
             FavortiesScreen(),
           ],
-        ),
-      ),
+        ), */
+      //),
     );
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -54,7 +54,7 @@ String get affordabilityText{
 }
 
   void selectMeal(BuildContext ctx) {
-    Navigator.of(ctx).pushNamed(MealDetailScreen.routeName, arguments: id);
+    Navigator.of(ctx).pushNamed(MealDetailScreen.routeName, arguments: id).then((result) => print(result));
   }
 
   @override
